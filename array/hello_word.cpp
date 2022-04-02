cd3accdd6fc5ee4adde9cd410eca619b03f6b9db
Lzrui <31103015+Lzrui@users.noreply.github.com>
2022-04-02 23:18:55 +0800

    Update hello_word.cpp

--- array/hello_word.cpp
+++ array/hello_word.cpp
@@ -1,6 +1,14 @@
-#include<iostream>
+// #include<iostream>
 
-int main(){
-  std::cout << "Hello,world!" << std::endl;
-  return 0;
+// int main(){
+//   std::cout << "Hello,world!" << std::endl;
+//   return 0;
+// }
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
 }
