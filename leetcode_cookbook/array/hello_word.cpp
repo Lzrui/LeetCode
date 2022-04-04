5b2983aac9b9d312b3d65668fdde21a6232c8972
Lzrui <1573400919@qq.com>
2022-04-04 12:16:16 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	array/hello_word.cpp

--- leetcode_cookbook/array/hello_word.cpp
+++ leetcode_cookbook/array/hello_word.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
- 
+ class A{
+ private:
+     int a;
+     static int b;
+ };
+int A::b = 5;
 int main() 
 {
+    A c;
     cout << "Hello, World!";
     return 0;
 }
