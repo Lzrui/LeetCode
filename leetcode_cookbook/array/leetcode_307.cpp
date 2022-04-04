b97c20a25f9f443b43d6baadce6da7bed29b17a5
Lzrui <1573400919@qq.com>
2022-04-04 16:47:03 +0800

    Merge remote-tracking branch 'origin/main'

--- leetcode_cookbook/array/leetcode_307.cpp
+++ leetcode_cookbook/array/leetcode_307.cpp
@@ -103,7 +103,7 @@ public:
 };
 
 int main(){
-    vector<int> nums = {1,3,5};
+    vector<int> nums = {1,2,3,4,5,6,7,8,9,10,11,12};
     NumArray test(nums);
     test.update(1,10);
     cout << test.sumRange(0,2) << endl;
