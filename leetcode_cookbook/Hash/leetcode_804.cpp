80d685b203cd5d60dbb65e7b4b37cbae11851ccf
Lzrui <1573400919@qq.com>
2022-04-10 18:43:17 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ leetcode_cookbook/Hash/leetcode_804.cpp
@@ -0,0 +1,38 @@
+//
+// Created by l50021384 on 2022/4/6.
+//
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+//反向计算
+const static string MORSE[] = {
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+        "....", "..", ".---", "-.-", ".-..", "--", "-.",
+        "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+        "...-", ".--", "-..-", "-.--", "--.."
+};
+
+class Solution {
+public:
+    static int uniqueMorseRepresentations(vector<string> &words) {
+        unordered_set<string> seen;
+        for (auto &word: words) {
+            string code;
+            for (auto &c: word) {
+                code.append(MORSE[c - 'a']);
+            }
+            seen.emplace(code);
+        }
+        return seen.size();
+    }
+};
+
+int main(){
+    vector<string> words{"gin", "zen", "gig", "msg"};
+  cout << Solution::uniqueMorseRepresentations(words) << endl;
+  return 0;
+}
+
+
+
