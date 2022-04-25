5d18a6ad5e3ecf42c8175d275d32e3ac3a360660
Lzrui <1573400919@qq.com>
2022-04-25 10:01:33 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ leetcode_101/Greedy/Candy.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  static int candy(vector<int>& ratings) {
+    int n = ratings.size();
+    if(n < 2){
+      return n;
+    }
+    vector<int> res(n,1);
+    for(int i = 1;i<n;++i){
+      if(ratings[i] > ratings[i-1]){
+        res[i] = res[i-1]+1;
+      }
+    }
+    for(int i = n-1;i>0;--i){
+      if(ratings[i] < ratings[i-1]){
+        res[i-1] = max(res[i]+1,res[i-1]);
+      }
+    }
+    return accumulate(res.begin(),res.end(),0);
+  }
+};
+
+int main()
+{
+  vector<int> ratings = {1,2,2};
+  cout << Solution::candy(ratings) << endl;
+  return 0;
+}
