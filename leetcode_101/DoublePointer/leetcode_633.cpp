86b1f60edabbec3a7df7e427728f66905d16a9b6
Lzrui <1573400919@qq.com>
2022-05-20 10:53:19 +0800

    Merge remote-tracking branch 'origin/main'

--- leetcode_101/DoublePointer/leetcode_633.cpp
+++ leetcode_101/DoublePointer/leetcode_633.cpp
@@ -2,52 +2,52 @@
 
 using namespace std;
 //双指针
-//class Solution {
-// public:
-//  static bool judgeSquareSum(int c) {
-//    long left = 0;
-//    long right = (int)sqrt(c);
-//    while (left <= right) {
-//      long sum = left * left + right * right;
-//      if (sum == c) {
-//        return true;
-//      } else if (sum > c) {
-//        right--;
-//      } else {
-//        left++;
-//      }
-//    }
-//    return false;
-//  }
-//};
-//数学
 class Solution {
  public:
   static bool judgeSquareSum(int c) {
-    for (int base = 2; base * base <= c; base++) {
-      // 如果不是因子，枚举下一个
-      if (c % base != 0) {
-        continue;
-      }
-
-      // 计算 base 的幂
-      int exp = 0;
-      while (c % base == 0) {
-        c /= base;
-        exp++;
-      }
-
-      // 根据 Sum of two squares theorem 验证
-      if (base % 4 == 3 && exp % 2 != 0) {
-        return false;
+    long left = 0;
+    long right = (int)sqrt(c);
+    while (left <= right) {
+      long sum = left * left + right * right;
+      if (sum == c) {
+        return true;
+      } else if (sum > c) {
+        right--;
+      } else {
+        left++;
       }
     }
-
-    // 例如 11 这样的用例，由于上面的 for 循环里 base * base <= c ，base == 11 的时候不会进入循环体
-    // 因此在退出循环以后需要再做一次判断
-    return c % 4 != 3;
+    return false;
   }
 };
+//数学
+//class Solution {
+// public:
+//  static bool judgeSquareSum(int c) {
+//    for (int base = 2; base * base <= c; base++) {
+//      // 如果不是因子，枚举下一个
+//      if (c % base != 0) {
+//        continue;
+//      }
+//
+//      // 计算 base 的幂
+//      int exp = 0;
+//      while (c % base == 0) {
+//        c /= base;
+//        exp++;
+//      }
+//
+//      // 根据 Sum of two squares theorem 验证
+//      if (base % 4 == 3 && exp % 2 != 0) {
+//        return false;
+//      }
+//    }
+//
+//    // 例如 11 这样的用例，由于上面的 for 循环里 base * base <= c ，base == 11 的时候不会进入循环体
+//    // 因此在退出循环以后需要再做一次判断
+//    return c % 4 != 3;
+//  }
+//};
 //使用sqrt函数
 //class Solution {
 // public boolean judgeSquareSum(int c) {
