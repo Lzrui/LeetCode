cbe6f40fdfd30e50484f6c769a351acc7fd929ac
Lzrui <1573400919@qq.com>
2022-05-23 17:21:20 +0800

    Merge remote-tracking branch 'origin/main'

--- test.cpp
+++ test.cpp
@@ -1,20 +1,40 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
+//二分查找
+//class Solution {
+// public:
+//  static int mySqrt(int x) {
+//    int l = 0, r = x, ans = -1;
+//    while (l <= r) {
+//      int mid = l + (r - l) / 2;
+//      if ((long long)mid * mid <= x) {
+//        ans = mid;
+//        l = mid + 1;
+//      } else {
+//        r = mid - 1;
+//      }
+//    }
+//    return ans;
+//  }
+//};
+//牛顿迭代法
 class Solution {
  public:
   static int mySqrt(int x) {
-    int l = 1,r = x;
-    while(l <= r){
-      int mid = l+(r-l)/2;
-      if(mid * mid == x)
-        return  mid;
-      else if(mid *mid > x)
-        r = mid-1;
-      else l = mid+1;
+    if (x == 0) {
+      return 0;
     }
-    return r;
+
+    double C = x, x0 = x;
+    while (true) {
+      double xi = 0.5 * (x0 + C / x0);
+      if (fabs(x0 - xi) < 1e-7) {
+        break;
+      }
+      x0 = xi;
+    }
+    return int(x0);
   }
 };
 
