cb314767d46e54b140a550dfdd14c7478edc8468
Lzrui <1573400919@qq.com>
2022-06-06 10:51:46 +0800

    Merge remote-tracking branch 'origin/main'

--- leetcode_cookbook/Array/leetcode_732.cpp
+++ leetcode_cookbook/Array/leetcode_732.cpp
@@ -2,27 +2,6 @@
 
 using namespace std;
 
-//差分数组
-//class MyCalendarThree {
-// public:
-//  MyCalendarThree() {
-//
-//  }
-//  int book(int start, int end) {
-//    int ans = 0;
-//    int maxBook = 0;
-//    cnt[start]++;
-//    cnt[end]--;
-//    for (auto &[_, freq] : cnt) {
-//      maxBook += freq;
-//      ans = max(maxBook, ans);
-//    }
-//    return ans;
-//  }
-// private:
-//  map<int, int> cnt;
-//};
-//线段树
 class MyCalendarThree {
  public:
   unordered_map<int, pair<int, int>> tree;
