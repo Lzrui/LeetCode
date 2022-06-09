04620e4fa03fbf350c7e1d11d3969998dec9faa0
Lzrui <1573400919@qq.com>
2022-06-09 12:01:44 +0800

    Merge remote-tracking branch 'origin/main'

--- test.cpp
+++ test.cpp
@@ -4,15 +4,39 @@ using namespace std;
 
 class Solution {
  public:
-  static bool isBoomerang(vector<vector<int>>& points) {
-    vector<int> v1 = {points[1][0] - points[0][0], points[1][1] - points[0][1]};
-    vector<int> v2 = {points[2][0] - points[0][0], points[2][1] - points[0][1]};
-    return v1[0] * v2[1] - v1[1] * v2[0] != 0;
+  Solution(vector<vector<int>>& rects) : rects{rects} {
+    this->arr.emplace_back(0);
+    for (auto & rect : rects) {
+      this->arr.emplace_back(arr.back() + (rect[2] - rect[0] + 1) * (rect[3] - rect[1] + 1));
+    }
   }
+
+  vector<int> pick() {
+    uniform_int_distribution<int> dis(0, arr.back() - 1);
+    int k = dis(gen);
+    int rectIndex = upper_bound(arr.begin(), arr.end(), k) - arr.begin() - 1;
+    k = k - arr[rectIndex];
+    int a = rects[rectIndex][0], b = rects[rectIndex][1];
+    int y = rects[rectIndex][3];
+    int col = y - b + 1;
+    int da = k / col;
+    int db = k - col * da;
+    return {a + da, b + db};
+  }
+ private:
+  vector<int> arr;
+  vector<vector<int>>& rects;
+  mt19937 gen{random_device{}()};
 };
 
+
 int main(){
-  vector<vector<int>> points = {{1,1},{2,3},{3,2}};
-  cout << Solution::isBoomerang(points) << endl;
+  vector<vector<int>> rects{{-2,-2,1,1},{2,2,4,6}};
+  Solution solution(rects);
+  vector<int> res = solution.pick(); // 返回 [1, -2]
+  res = solution.pick(); // 返回 [1, -1]
+  res = solution.pick(); // 返回 [-1, -2]
+  res = solution.pick(); // 返回 [-2, -2]
+  res = solution.pick(); // 返回 [0, 0]
   return 0;
 }
