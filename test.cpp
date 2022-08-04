d04ecb19758c860a517f23e4f1a8b43d7e087c2f
Lzrui <1573400919@qq.com>
2022-08-04 15:51:13 +0800

    Merge remote-tracking branch 'origin/main'

--- test.cpp
+++ test.cpp
@@ -15,9 +15,6 @@ class Solution {
 };
 
 int main(){
-  auto lam =[]() -> int { cout << "Hello, World!"; return 88; };
-  auto ret = lam();
-  cout<<ret<<endl; // ᬌڊ88
   int n = 5;
   vector<int> res = Solution::countBits(n);
   return 0;
