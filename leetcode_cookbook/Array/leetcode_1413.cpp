fac48bd316eab406effac304629c541cdbcf74a4
Lzrui <1573400919@qq.com>
2022-08-12 14:09:46 +0800

    Merge remote-tracking branch 'origin/main'

--- leetcode_cookbook/Array/leetcode_1413.cpp
+++ leetcode_cookbook/Array/leetcode_1413.cpp
@@ -5,14 +5,15 @@ using namespace std;
 class Solution {
  public:
   static int minStartValue(vector<int>& nums) {
-    partial_sum(nums.begin(), nums.end(), nums.begin());
-    int n = nums.size(), res = 1;
-    for (int i = 0; i < n; ++i) {
-      res = max(res, 1 - nums[i]);
+    int accSum = 0, accSumMin = 0;
+    for (int num : nums) {
+      accSum += num;
+      accSumMin = min(accSumMin, accSum);
     }
-    return res;
+    return -accSumMin + 1;
   }
 };
+
 
 int main(){
   vector<int> nums = {-3,2,-3,4,2};
