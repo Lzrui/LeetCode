16c8aec0f1c7c5671fac78bb6c9c24d84f1a1d09
Lzrui <1573400919@qq.com>
2022-08-24 17:30:02 +0800

    Merge remote-tracking branch 'origin/main'

--- leetcode_101/String/leetcode_647.cpp
+++ leetcode_101/String/leetcode_647.cpp
@@ -2,23 +2,6 @@
 
 using namespace std;
 
-//class Solution {
-// public:
-//  static bool isIsomorphic(string s, string t) {
-//    unordered_map<char, char> hash_st;
-//    unordered_map<char, char> hash_ts;
-//    int n = s.size();
-//    for (int i = 0; i < n; ++i) {
-//      if ((hash_st.count(s[i]) && hash_st[s[i]] != t[i]) ||
-//          (hash_ts.count(t[i]) && hash_ts[t[i]] != s[i]))
-//        return false;
-//      hash_st[s[i]] = t[i];
-//      hash_ts[t[i]] = s[i];
-//    }
-//    return true;
-//  }
-//};
-
 class Solution {
  public:
   static int countSubstrings(string s) {
