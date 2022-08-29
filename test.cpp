dfb8614530ae350154bbffd051623c9d88e39dc1
Lzrui <1573400919@qq.com>
2022-08-29 14:53:16 +0800

    Merge remote-tracking branch 'origin/main'

--- test.cpp
+++ test.cpp
@@ -2,65 +2,41 @@
 
 using namespace std;
 
-//暴力法
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     explicit ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+//迭代法
 //class Solution {
 // public:
-//  static int strStr(string haystack, string needle) {
-//    int m = haystack.size(), n = needle.size();
-//    for (int i = 0; i + n <= m; ++i) {
-//      bool flag = true;
-//      for (int j = 0; j < n; ++j) {
-//        if (haystack[i + j] != needle[j]) {
-//          flag = false;
-//          break;
-//        }
-//      }
-//      if (flag)
-//        return i;
+//  ListNode* reverseList(ListNode* head) {
+//    ListNode *prev = nullptr, *next;
+//    while (head) {
+//      next = head->next;
+//      head->next = prev;
+//      prev = head;
+//      head = next;
 //    }
-//    return -1;
+//    return prev;
 //  }
 //};
-
-// KMP算法
+//递归法
 class Solution {
  public:
-  static int strStr(string haystack, string needle) {
-    int m = haystack.size(), n = needle.size();
-    if (n == 0) return 0;
-    vector<int> next(n, -1);
-    calNext(needle, next);
-    for (int i = 0, k = -1; i < m; ++i) {
-      while (k > -1  && needle[k+1] != haystack[i]) {
-        k = next[k];
-      }
-      if (needle[k+1] == haystack[i]) {
-        ++k;
-      }
-      if (k == n - 1) {
-        return i - n + 1;
-      }
-    }
-    return -1;
+  ListNode* reverseList(ListNode* head, ListNode *prev = nullptr) {
+    if (!head)
+      return prev;
+    ListNode *next = head->next;
+    head->next = prev;
+    return reverseList(next, head);
   }
-
-  static void calNext(const string &needle, vector<int> &next) {
-    for (int j = 1, p = -1; j <  needle.size(); ++j) {
-      while (p > -1 && needle[p+1] != needle[j]) {
-        p = next[p];
-      }
-      if (needle[p+1] == needle[j]) {
-        ++p;
-      }
-      next[j] = p;
-    }
-  }
-
 };
 
 int main() {
-  string haystack("ababadababababc"), needle("ababc");
-  int res = Solution::strStr(haystack, needle);
   return 0;
 }
 
