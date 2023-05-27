f05b40988f3d7374c56db9e305ae1caf0413c38c
Administrator <1573400919@qq.com>
2023-05-27 12:26:29 +0800

    Merge remote-tracking branch 'origin/main'

--- test.cpp
+++ test.cpp
@@ -4,22 +4,39 @@ using namespace std;
 
 class Solution {
  public:
-  vector<int> constructArray(int n, int k) {
-    vector<int> ans;
-    for (int i = 1; i < n - k; ++i) {
-      ans.emplace_back(i);
-    }
-    for (int i  = n - k, j = n; i <= j; ++i, --j) {
-      ans.emplace_back(i);
-      if (i != j) {
-        ans.emplace_back(j);
+  static vector<vector<int>> generateMatrix(int n) {
+    vector<vector<int>> ans(n, vector<int>(n));
+    int loop = n / 2;
+    int start = 0;
+    int offset = 1;
+    int val = 1;
+    int i, j;
+    while (loop--) {
+      for (j = start; j < n - offset; ++j) {
+        ans[start][j] = val++;
       }
+      for (i = start; i < n - offset; ++i) {
+        ans[i][j] = val++;
+      }
+      for (; j > start; --j) {
+        ans[i][j] = val++;
+      }
+      for (; i > start; --i) {
+        ans[i][j] = val++;
+      }
+      ++start;
+      ++offset;
+    }
+    if ((n & 1) == 1) {
+      ans[n/2][n/2] = val;
     }
     return ans;
   }
 };
 
 int main() {
-  return 0;
+    string s("dasd");
+    vector<vector<int>> ans = Solution::generateMatrix(4);
+    return 0;
 }
 
